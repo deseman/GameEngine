a6469bb08bf6340fa5ea7647fd877d6c7722bf13
Martijn Vermeulen <martijn@nexios.com>
2022-07-13 19:52:50 +0200

    Added Event

---
+++ GameEngine/src/GameEngine/Events/KeyEvent.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace GameEngine {
+
+	class GE_API KeyEvent : public Event
+	{
+	protected:
+		int m_KeyCode;
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode) {}
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	};
+
+	class GE_API KeyPressedEvent : public KeyEvent
+	{
+	private:
+		int m_RepeatCount;
+	public:
+		KeyPressedEvent(int keycode, int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: keycode=" << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+	};
+
+	class GE_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: keycode=" << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
+}
---
+++ GameEngine/src/GameEngine/Events/MouseEvent.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace GameEngine {
+
+	class GE_API MouseMovedEvent : public Event
+	{
+	private:
+		float m_MouseX, m_MouseY;
+	public:
+		MouseMovedEvent(float x, float y)
+			: m_MouseX(x), m_MouseY(y) {}
+
+		inline float GetX() const { return m_MouseX; }
+		inline float GetY() const { return m_MouseY; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseMovedEvent: x=" << m_MouseX << ", y=" << m_MouseY;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseMoved)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+	};
+
+	class GE_API MouseScrolledEvent : public Event
+	{
+	private:
+		float m_XOffset, m_YOffset;
+	public:
+		MouseScrolledEvent(float xOffset, float yOffset)
+			: m_XOffset(xOffset), m_YOffset(yOffset) {}
+
+		inline float GetXOffset() const { return m_XOffset; }
+		inline float GetYOffset() const { return m_YOffset; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseScrolledEvent: xOffset=" << GetXOffset() << ", yOffset=" << GetYOffset();
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseScrolled)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+	};
+
+	class GE_API MouseButtonEvent : public Event
+	{
+	protected:
+		int m_Button;
+		MouseButtonEvent(int button)
+			: m_Button(button) {}
+	public:
+		inline int GetMouseButton() const { return m_Button; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+	};
+
+	class GE_API MouseButtonPressedEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonPressedEvent(int button)
+			: MouseButtonEvent(button) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseButtonPressed: button=" << m_Button;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseButtonPressed)
+	};
+
+	class GE_API MouseButtonReleasedEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonReleasedEvent(int button)
+			: MouseButtonEvent(button) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseButtonReleased: " << m_Button;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseButtonReleased)
+	};
+}
