0d0c929aef5b42d97ee8592f18fe7b6b92aad318
Martijn Vermeulen <martijn@nexios.com>
2022-07-14 15:16:26 +0200

    Added Glad

--- GameEngine/src/Platform/Windows/WindowsWindow.cpp
+++ GameEngine/src/Platform/Windows/WindowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "GameEngine/Events/MouseEvent.h"
 #include "GameEngine/Events/KeyEvent.h"
 
+#include <glad/glad.h>
+
 namespace GameEngine {
 
 	static bool s_GLFWInitialized = false;
@@ -49,6 +51,8 @@ namespace GameEngine {
 
 		m_Window = glfwCreateWindow((int)m_Data.Width, (int)m_Data.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		GE_ASSERT(status, "Failed to initialize Glad!");
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
