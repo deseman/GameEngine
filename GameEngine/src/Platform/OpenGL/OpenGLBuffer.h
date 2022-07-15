cd59997d7e2fe2a7852955578632080d885b7c0d
Martijn Vermeulen <martijn@nexios.com>
2022-07-15 19:26:04 +0200

    Added Buffer layout

--- GameEngine/src/Platform/OpenGL/OpenGLBuffer.h
+++ GameEngine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -8,12 +8,16 @@ namespace GameEngine {
 	{
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	public:
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
