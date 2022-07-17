7ef6cdfcb133a704759fbf3eb7afb1f2c63a60b9
Martijn Vermeulen <martijn@nexios.com>
2022-07-17 14:49:32 +0200

    Updated shader

--- GameEngine/src/GameEngine/Renderer/Shader.h
+++ GameEngine/src/GameEngine/Renderer/Shader.h
@@ -16,6 +16,7 @@ namespace GameEngine {
 		void Bind() const;
 		void Unbind() const;
 
+		void UploadUniformFloat4(const std::string& name, const glm::vec4& values);
 		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
 	};
 }
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,7 +1,8 @@
 #include <GameEngine.h>
 
 #include "imgui/imgui.h"
-#include "GameEngine.h"
+
+#include <glm/gtc/matrix_transform.hpp>
 
 class ExampleLayer : public GameEngine::Layer
 {
@@ -34,10 +35,10 @@ public:
 		m_SquareVA.reset(GameEngine::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<GameEngine::VertexBuffer> squareVB;
@@ -59,6 +60,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -67,7 +69,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -88,40 +90,44 @@ public:
 
 		m_Shader.reset(new GameEngine::Shader(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec4 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = u_Color;
 			}
 		)";
 
-		m_BlueShader.reset(new GameEngine::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(new GameEngine::Shader(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(GameEngine::Timestep ts) override
 	{
+		// Camera movement
 		if (GameEngine::Input::IsKeyPressed(GE_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
 		else if (GameEngine::Input::IsKeyPressed(GE_KEY_RIGHT))
@@ -137,11 +143,11 @@ public:
 		else if (GameEngine::Input::IsKeyPressed(GE_KEY_DOWN))
 			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
+		// Camera rotation
 		if (GameEngine::Input::IsKeyPressed(GE_KEY_A))
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 		else if (GameEngine::Input::IsKeyPressed(GE_KEY_D))
 			m_CameraRotation += m_CameraRotationSpeed * ts;
-		
 
 		GameEngine::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		GameEngine::RenderCommand::Clear();
@@ -151,7 +157,25 @@ public:
 
 		GameEngine::Renderer::BeginScene(m_Camera);
 
-		GameEngine::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale (glm::mat4(1.0f), glm::vec3(0.1f));
+
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				if (x % 2 == 0)
+					m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
+				else
+					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
+
+				GameEngine::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
+			}
+		}
+		
 		GameEngine::Renderer::Submit(m_Shader, m_VertexArray);
 
 		GameEngine::Renderer::EndScene();
@@ -169,7 +193,7 @@ private:
 	std::shared_ptr<GameEngine::Shader> m_Shader;
 	std::shared_ptr<GameEngine::VertexArray> m_VertexArray;
 
-	std::shared_ptr<GameEngine::Shader> m_BlueShader;
+	std::shared_ptr<GameEngine::Shader> m_FlatColorShader;
 	std::shared_ptr<GameEngine::VertexArray> m_SquareVA;
 
 	GameEngine::OrthographicCamera m_Camera;
